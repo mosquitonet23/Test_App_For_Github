1a977e2a50aa2d11ea23186f66e060774041587d
Welly Malquitar <wmalquitar23@gmail.com>
2015-10-15 17:53:24 +0800

    First Commit for Test App
    My First Commit - Testing

---
+++ Test_App_For_Github/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show("Welcome to the World!")
+    End Sub
+End Class
